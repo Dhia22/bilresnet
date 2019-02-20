69d452c70cbb0ccc1e81f98966b9d505ffaeb1f9
Dhia <meddhiabesbes@gmail.com>
2019-02-20 05:28:34 +0100

    ds

--- resnet_152_bil.py
+++ resnet_152_bil.py
@@ -13,6 +13,104 @@ from custom_layers.scale_layer import Scale
 import sys
 sys.setrecursionlimit(3000)
 
+import numpy as np
+import tensorflow as tf
+from keras import Input
+from keras.layers import Conv2D, MaxPooling2D, merge, Lambda, Dense, Reshape, regularizers
+
+ 
+def compact_bilinear(tensors_list):
+
+    def _generate_sketch_matrix(rand_h, rand_s, output_dim):
+        """
+        Return a sparse matrix used for tensor sketch operation in compact bilinear
+        pooling
+        Args:
+            rand_h: an 1D numpy array containing indices in interval `[0, output_dim)`.
+            rand_s: an 1D numpy array of 1 and -1, having the same shape as `rand_h`.
+            output_dim: the output dimensions of compact bilinear pooling.
+        Returns:
+            a sparse matrix of shape [input_dim, output_dim] for tensor sketch.
+        """
+
+        # Generate a sparse matrix for tensor count sketch
+        rand_h = rand_h.astype(np.int64)
+        rand_s = rand_s.astype(np.float32)
+        assert (rand_h.ndim == 1 and rand_s.ndim == 1 and len(rand_h) == len(rand_s))
+        assert (np.all(rand_h >= 0) and np.all(rand_h < output_dim))
+
+        input_dim = len(rand_h)
+        indices = np.concatenate((np.arange(input_dim)[..., np.newaxis],
+                                  rand_h[..., np.newaxis]), axis=1)
+        sparse_sketch_matrix = tf.sparse_reorder(
+            tf.SparseTensor(indices, rand_s, [input_dim, output_dim]))
+        return sparse_sketch_matrix
+
+    bottom1, bottom2 = tensors_list
+    output_dim = 8192
+
+    # Static shapes are needed to construction count sketch matrix
+    input_dim1 = bottom1.get_shape().as_list()[-1]
+    input_dim2 = bottom2.get_shape().as_list()[-1]
+
+    # print (bottom1.get_shape().as_list())
+    # print (bottom2.get_shape().as_list())
+
+    # Step 0: Generate vectors and sketch matrix for tensor count sketch
+    # This is only done once during graph construction, and fixed during each
+    # operation
+    seed_h_1 = 1
+    seed_s_1 = 3
+    seed_h_2 = 5
+    seed_s_2 = 7
+
+    # Generate sparse_sketch_matrix1 using rand_h_1 and rand_s_1
+    np.random.seed(seed_h_1)
+    rand_h_1 = np.random.randint(output_dim, size=input_dim1)
+    np.random.seed(seed_s_1)
+    rand_s_1 = 2 * np.random.randint(2, size=input_dim1) - 1
+    sparse_sketch_matrix1 = _generate_sketch_matrix(rand_h_1, rand_s_1, output_dim)
+
+    # Generate sparse_sketch_matrix2 using rand_h_2 and rand_s_2
+    np.random.seed(seed_h_2)
+    rand_h_2 = np.random.randint(output_dim, size=input_dim2)
+    np.random.seed(seed_s_2)
+    rand_s_2 = 2 * np.random.randint(2, size=input_dim2) - 1
+    sparse_sketch_matrix2 = _generate_sketch_matrix(rand_h_2, rand_s_2, output_dim)
+
+    # Step 1: Flatten the input tensors and count sketch
+    bottom1_flat = tf.reshape(bottom1, [-1, input_dim1])
+    bottom2_flat = tf.reshape(bottom2, [-1, input_dim2])
+
+    # Essentially:
+    #   sketch1 = bottom1 * sparse_sketch_matrix
+    #   sketch2 = bottom2 * sparse_sketch_matrix
+    # But tensorflow only supports left multiplying a sparse matrix, so:
+    #   sketch1 = (sparse_sketch_matrix.T * bottom1.T).T
+    #   sketch2 = (sparse_sketch_matrix.T * bottom2.T).T
+    sketch1 = tf.transpose(tf.sparse_tensor_dense_matmul(sparse_sketch_matrix1,
+                                                         bottom1_flat, adjoint_a=True, adjoint_b=True))
+    sketch2 = tf.transpose(tf.sparse_tensor_dense_matmul(sparse_sketch_matrix2,
+                                                         bottom2_flat, adjoint_a=True, adjoint_b=True))
+
+    # Step 2: FFT
+    fft1 = tf.fft(tf.complex(real=sketch1, imag=tf.zeros_like(sketch1)))
+    fft2 = tf.fft(tf.complex(real=sketch2, imag=tf.zeros_like(sketch2)))
+
+    # Step 3: Elementwise product
+    fft_product = tf.multiply(fft1, fft2)
+
+    # Step 4: Inverse FFT and reshape back
+    # Compute output shape dynamically: [batch_size, height, width, output_dim]
+    cbp_flat = tf.real(tf.ifft(fft_product))
+
+    output_shape = tf.add(tf.multiply(tf.shape(bottom1), [1, 1, 1, 0]),
+                          [0, 0, 0, output_dim])
+    cbp = tf.reshape(cbp_flat, output_shape)
+
+    # print (cbp.get_shape().as_list())
+
+    return cbp
 def identity_block(input_tensor, kernel_size, filters, stage, block):
     '''The identity_block is the block that has no conv layer at shortcut
     # Arguments
@@ -159,6 +257,16 @@ def resnet152_model(img_rows, img_cols, color_type=1, num_classes=None):
     # Truncate and replace softmax layer for transfer learning
     # Cannot use model.layers.pop() since model is not of Sequential() type
     # The method below works since pre-trained weights are stored in layers but not in the model
+	compact_bilinear_arg_list = [x, x]
+
+    output_shape_x = x.get_shape().as_list()[1:]
+    output_shape_cb = (output_shape_x[0], output_shape_x[1], 8000,)
+    x = merge(compact_bilinear_arg_list, mode=compact_bilinear, name='compact_bilinear', output_shape=output_shape_cb)
+
+    # Sign sqrt and L2 normalize result
+    x = Lambda(lambda x: K.sign(x) * K.sqrt(K.abs(x)))(x)
+    x = Lambda(lambda x: K.l2_normalize(x, axis=-1))(x)
+	
     x_newfc = AveragePooling2D((7, 7), name='avg_pool')(x)
     x_newfc = Flatten()(x_newfc)
     x_newfc = Dense(num_classes, activation='softmax', name='fc8')(x_newfc)
